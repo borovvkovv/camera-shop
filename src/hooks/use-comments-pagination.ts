ae1512b9be4a579c50e510416667e3f18f95a5fb
Dmitriy Borovkov <borovvkovv@gmail.comi>
2023-06-22 02:08:35 +0500

    Добавляет автозагрузку обзоров товаров при прокрутке вниз

--- src/hooks/use-comments-pagination.ts
+++ src/hooks/use-comments-pagination.ts
@@ -1,17 +1,41 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { COMMENTS_ON_PAGE } from '../const';
 import { Review } from '../types/review';
 
-export default function useCommentsPagination(
-  allComments: Review[]
-) {
-
+export default function useCommentsPagination(allComments: Review[]) {
   const [currentPage, setCurrentPage] = useState(1);
+
+  function handleWindowsScroll() {
+    let timerId: undefined | NodeJS.Timeout = undefined;
+    let isLoadingActive = false;
+
+    return () => {
+      if (
+        !isLoadingActive &&
+        document.scrollingElement &&
+        window.innerHeight + document.documentElement.scrollTop >=
+          document.scrollingElement?.scrollHeight - 10
+      ) {
+        isLoadingActive = true;
+        timerId = setTimeout(() => {
+          setCurrentPage((prev) => prev + 1);
+          isLoadingActive = false;
+        }, 1000);
+      } else if (timerId) {
+        clearTimeout(timerId);
+        isLoadingActive = false;
+      }
+    };
+  }
 
   const pagedComments = useMemo(
     () => allComments.slice(0, currentPage * COMMENTS_ON_PAGE),
     [allComments, currentPage]
   );
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleWindowsScroll());
+  }, []);
 
   const maxPageNumber = Math.ceil(allComments.length / COMMENTS_ON_PAGE);
 

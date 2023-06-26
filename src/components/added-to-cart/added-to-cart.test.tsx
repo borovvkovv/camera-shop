1fb8acab1effc9bdc2a93c8406b85049b9583634
Dmitriy Borovkov <borovvkovv@gmail.comi>
2023-06-26 23:13:12 +0500

    Добавляет функционал к тесту компонента AddedToCard

--- src/components/added-to-cart/added-to-cart.test.tsx
+++ src/components/added-to-cart/added-to-cart.test.tsx
@@ -1,38 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import { Route, Routes } from 'react-router';
 import HistoryRouter from '../history-router/history-router';
 import AddedToCart from './added-to-cart';
 
 const history = createMemoryHistory();
 const setVisibility = jest.fn();
 
-const fakeApp = (
-  <HistoryRouter history={history}>
-    <Routes>
-      <Route
-        path='*'
-        element={
-          <AddedToCart
-            isVisible
-            setVisibility={setVisibility}
-          />
-        }
-      />
-    </Routes>
-  </HistoryRouter>
-);
-
 describe('Component: AddedToCart', () => {
   it('should render correctly', () => {
-    history.push('/fake');
+    const fakeApp = (
+      <HistoryRouter history={history}>
+        <AddedToCart
+          isVisible
+          setVisibility={setVisibility}
+        />
+      </HistoryRouter>
+    );
 
     render(fakeApp);
 
+    expect(screen.getByTestId('addedToCartPopup')).toHaveClass('is-active');
     expect(
       screen.getByText(/Товар успешно добавлен в корзину/)
     ).toBeInTheDocument();
     expect(screen.getByText(/Продолжить покупки/)).toBeInTheDocument();
     expect(screen.getByText(/Перейти в корзину/)).toBeInTheDocument();
+  });
+
+  it('should hide popup when prop isVisible=false', () => {
+    const fakeApp = (
+      <HistoryRouter history={history}>
+        <AddedToCart
+          isVisible={false}
+          setVisibility={setVisibility}
+        />
+      </HistoryRouter>
+    );
+
+    render(fakeApp);
+
+    expect(screen.getByTestId('addedToCartPopup')).not.toHaveClass('is-active');
   });
 });

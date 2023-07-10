7ab1fbf6f75d356911e8e83767781c081c599fcc
Dmitriy Borovkov <borovvkovv@gmail.comi>
2023-07-11 00:35:20 +0500

    Исправление ошибки - Переключение по результатам поиска не производится кликом на стрелки клавиатуры

--- src/hooks/use-search-form.ts
+++ src/hooks/use-search-form.ts
@@ -26,61 +26,67 @@ export default function useSearchForm(
       }
     }
 
-    function handleKeyDown(evt: KeyboardEvent) {
-      if (evt.key === 'Escape') {
-        setFormSearchOpened(false);
-        (evt.target as HTMLElement).blur();
-      }
+    function handleKeyDown() {
+      let currentFocusableIndex = 0;
+      return (evt: KeyboardEvent) => {
+        if (evt.key === 'Escape') {
+          setFormSearchOpened(false);
+          (evt.target as HTMLElement).blur();
+        }
 
-      if (modalRef.current !== null && evt.key === 'Tab') {
-        const target = evt.target as HTMLElement;
-        const focusables = modalRef.current.querySelectorAll(
-          '.btn,input,button,select,textarea,a,[tabindex]:not([tabindex="-1"])'
-        );
-
-        const firstElement = focusables[0] as HTMLElement;
-        const lastElement = focusables[focusables.length - 1] as HTMLElement;
-        const refClasses = Object.values(modalRef.current.classList)
-          .map((className) => `.${className}`)
-          .join('');
-
-        if (!target.closest(refClasses)) {
-          if (evt.shiftKey) {
-            target.blur();
-            lastElement.focus();
-            evt.preventDefault();
-          } else {
-            target.blur();
-            firstElement.focus();
-            evt.preventDefault();
-          }
-        } else {
-          if (evt.shiftKey) {
-            if (evt.target === firstElement) {
-              target.blur();
-              lastElement.focus();
-              evt.preventDefault();
-            }
-          } else {
-            if (evt.target === lastElement) {
-              target.blur();
-              firstElement.focus();
-              evt.preventDefault();
-            }
+        if (
+          modalRef.current !== null &&
+          (evt.key === 'ArrowUp' || (evt.key === 'Tab' && evt.shiftKey))
+        ) {
+          evt.preventDefault();
+          const focusablesExceptReset =
+            modalRef.current?.querySelectorAll('li,input');
+          if (currentFocusableIndex > 0) {
+            (
+              focusablesExceptReset[currentFocusableIndex] as HTMLElement
+            ).blur();
+            currentFocusableIndex -= 1;
+            (
+              focusablesExceptReset[currentFocusableIndex] as HTMLElement
+            ).focus();
           }
         }
-      }
+        if (
+          modalRef.current !== null &&
+          (evt.key === 'ArrowDown' || (evt.key === 'Tab' && !evt.shiftKey))
+        ) {
+          evt.preventDefault();
+          const focusablesExceptReset =
+            modalRef.current?.querySelectorAll('li,input');
+          if (focusablesExceptReset.length > currentFocusableIndex + 1) {
+            (
+              focusablesExceptReset[currentFocusableIndex] as HTMLElement
+            ).blur();
+            currentFocusableIndex += 1;
+            (
+              focusablesExceptReset[currentFocusableIndex] as HTMLElement
+            ).focus();
+          }
+          evt.preventDefault();
+        }
+      };
     }
 
+    const handleKeyDownClosure = handleKeyDown();
+
     if (isFormSearchOpened) {
-      document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('keydown', handleKeyDownClosure);
       document.addEventListener('click', handleClick, true);
     }
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keydown', handleKeyDownClosure);
       document.removeEventListener('click', handleClick, true);
     };
-  }, [isFormSearchOpened, modalRef, searchPattern]);
+  }, [
+    isFormSearchOpened,
+    modalRef,
+    searchPattern,
+  ]);
 
   return {
     isFormSearchOpened,
